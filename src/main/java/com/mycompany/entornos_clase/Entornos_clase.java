bce3c3015e11025710b9bb0c35616167931e4a60
Oscar Montaña <oscar.mongar@educa.jcyl.es>
2024-04-25 17:33:24 +0200

    Primer comit del Entornos clase

---
+++ src/main/java/com/mycompany/entornos_clase/Entornos_clase.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.entornos_clase;
+
+/**
+ *
+ * @author Admin
+ */
+public class Entornos_clase {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("PRUEBA EN CLASE");
+        System.out.println("COMO SUBIR TODO!");
+    }
+}
